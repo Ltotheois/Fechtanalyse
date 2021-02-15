b546973e286cc054120d85e9735a39c25d97b329
Luis <64982212+Ltotheois@users.noreply.github.com>
2021-02-15 11:44:43 +0100

    SW Updates

--- sw.js
+++ sw.js
@@ -1,20 +1,23 @@
+// version 1.0
+// change version to reinstall service worker and update Files
+
 var cacheName = 'cache_fechtanalyse';
 var filesToCache = [
-  '/Fechtanalyse/',
-  '/Fechtanalyse/fontawesome/css/all.css',
-  '/Fechtanalyse/favicon.svg',
-  '/Fechtanalyse/stylesheet.css',
-  '/Fechtanalyse/Fechten.html',
-  '/Fechtanalyse/sw.js',
-  '/Fechtanalyse/manifest.json',
-  '/Fechtanalyse/fontawesome/webfonts/fa-regular-400.woff2',
-  '/Fechtanalyse/roboto/roboto-v20-latin-regular.woff2',
-  '/Fechtanalyse/fontawesome/webfonts/fa-solid-900.woff2',
-  '/Fechtanalyse/title.png'
+	'/Fechtanalyse/',
+	'/Fechtanalyse/fontawesome/css/all.css',
+	'/Fechtanalyse/favicon.svg',
+	'/Fechtanalyse/stylesheet.css',
+	'/Fechtanalyse/Fechten.html',
+	'/Fechtanalyse/manifest.json',
+	'/Fechtanalyse/fontawesome/webfonts/fa-regular-400.woff2',
+	'/Fechtanalyse/roboto/roboto-v20-latin-regular.woff2',
+	'/Fechtanalyse/fontawesome/webfonts/fa-solid-900.woff2',
+	'/Fechtanalyse/title.png'
 ];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
+  caches.delete(cacheName);
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
